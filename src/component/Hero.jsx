c236b883d341eb2463b4eba0e71fef2d50b33fa9
sutton <50450947+Ioome@users.noreply.github.com>
2023-05-29 23:55:58 +0800

    sutton
    增加

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -35,7 +35,21 @@ const Hero = () => {
                       rounded-3xl border-4 border-secondary
                       flex justify-center items-start p-2
                 ">
-
+                    <motion.div
+                        animate={{
+                            y: [0,24,0]
+                        }}
+                        transition={
+                            {
+                                duration:1.5,
+                                repeat:Infinity,
+                                repeatType:'loop'
+                            }
+                        }
+                        className="w-3 h-3 rounded-full
+                            bg-secondary mb-1
+                        "
+                    />
 
                 </div>
             </a>
