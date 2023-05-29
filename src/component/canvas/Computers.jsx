7c38ee4c33b18ca3ea9a1eb0d30e8cea688e7cad
devil <50450947+Ioome@users.noreply.github.com>
2023-05-29 11:34:51 +0000

    Update Comuters.jsx add  <hemisphereLight intensity={0.15} groundColor="black" />       <pointLight intensity={1} />       <primitive object={computer.scene} />

--- src/component/canvas/Computers.jsx
+++ src/component/canvas/Computers.jsx
@@ -1,26 +1,20 @@
-import React, {Suspense, useEffect, useState} from 'react'
-import {Canvas} from "@react-three/fiber";
-import {OrbitControls, Preload, useGLTF} from "@react-three/drei";
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 const Computers = () => {
-    const computer = useGLTF('./desktop_pc/scene.gltf')
-    return (
-            <mesh>
-                <hemisphereLight />
-            </mesh>
-    )
-}
+  const computer = useGLTF("./desktop_pc/scene.gltf");
+  return (
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <pointLight intensity={1} />
+      <primitive object={computer.scene} />
+    </mesh>
+  );
+};
 
-const ComputerCanvas=()=>{
-    return (
-        <Canvas
-            frameloop="demand"
-            shadows
-            camera
-        >
+const ComputerCanvas = () => {
+  return <Canvas frameloop="demand" shadows camera></Canvas>;
+};
 
-        </Canvas>
-    )
-}
-
-export default Computers
+export default Computers;
